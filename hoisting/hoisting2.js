34b91a74aaa1da3179f9b567c394510e92ef386a
gabriel <gabrielcmoris@gmail.com>
2022-08-30 10:10:43 +0200

    hoisting

--- hoisting/hoisting2.js
+++ hoisting/hoisting2.js
@@ -1,5 +1,8 @@
+//when the global execution context starts, the javascript engine hoists both variables as undefined.
+
 var favouriteFood = "grapes";
 var foodThoughts = function () {
+  //Inside this execution context Hoisting happens again, so favouriteFood is undefined again!
   console.log("Original favourite food : ", favouriteFood);
 
   var favouriteFood = "sushi";
