fdcd8be2976c8a7eb8105ba19ddc2be616bc2657
gabriel <gabrielcmoris@gmail.com>
2022-09-01 15:25:12 +0200

    types

--- types/types.js
+++ types/types.js
@@ -1,3 +1,4 @@
+//PRIMITIVE
 const number = 5;
 const boolean = true;
 const string = "string";
@@ -7,8 +8,12 @@ const typeUndefined = undefined;
 const typeNull = null;
 //symbol is used for object properties to make them unique
 const symbol = Symbol("just a symbol");
+
+//NOT PRIMITIVE
 const object = {};
+const objectAgain = [];
 //The array type as the function type is an object. However, if I do typeof function(){} it shows function
+const objectToo = function () {};
 
 console.log(
   typeof number,
@@ -17,5 +22,24 @@ console.log(
   typeof typeUndefined,
   typeof typeNull,
   typeof symbol,
-  typeof object
+  typeof object,
+  typeof objectAgain,
+  typeof objectToo
 );
+
+//object types: Example:
+const string1 = true.toString();
+//A primitive Value doesn't have a method. In the background the engine makes this:
+const string2 = Boolean(true).toString();
+console.log(
+  "normal type: ",
+  typeof string1,
+  "javaScript engine: ",
+  typeof string2
+);
+
+//------------------------------//
+
+let arr = ["1", "2", "3"];
+//For JS this is the same as:
+let arr2 = { 0: "1", 1: "2", 2: "3" };
