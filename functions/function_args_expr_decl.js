5a72fac63559c0dc65a99972c7e2ca35dc7b1507
gabriel <gabrielcmoris@gmail.com>
2022-09-02 14:03:23 +0200

    functions

--- functions/function_args_expr_decl.js
+++ functions/function_args_expr_decl.js
@@ -27,4 +27,63 @@ console.log(marry2("Pepe", "Maruja"));
 //function constructor
 
 const funcConst = new Function("return 34");
-console.log("funcconst", funcConst());
+console.log("funcconst", funcConst()); //34
+
+//it accepts also parameters
+const funcConst2 = new Function("param", "return param * 2");
+console.log("funcconst2", funcConst2(2)); //4
+
+//Callable object!!: if I add a meythod to a function The enginr
+//will make an special object
+
+function wohoo() {
+  console.log("wooooooohoooo");
+}
+
+wohoo.yell = "WOOOOOOOOHOOOOOO";
+
+//the javaScript creates this
+
+const specialObject = {
+  yell: "WOOOOOOOOHOOOOOO",
+  name: "wohoo",
+  //and also adds something like this
+  // ():console.log("wohooo")
+};
+
+//Funcions are first class citizens in JS
+//1. can be asigned to variables and props ob objects
+var variable = function () {};
+//2. can be pases as arguments in another function
+function one(arg1, func1) {
+  func1(arg1);
+}
+one("I am loged!", console.log);
+//3 We can return functions as a values from other functions
+function b() {
+  return function c() {
+    console.log("returned a function here!");
+  };
+}
+
+b()();
+//or also:
+var c = b();
+c();
+
+//careful for performance this inicialices the function each time!
+
+for (i = 0; i <= 5; i++) {
+  function f(b) {
+    console.log(b);
+  }
+  f(i);
+}
+//better the function initialices before and then I call it
+
+function g(d) {
+  console.log(d);
+}
+for (j = 0; j <= 6; j++) {
+  g(j);
+}
