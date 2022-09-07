9ee9f043c03466efd08929abdc6c6071e1ef1bef
gabriel <gabrielcmoris@gmail.com>
2022-09-07 17:39:27 +0200

    closures and memory

--- functions/closures.js
+++ functions/closures.js
@@ -27,7 +27,7 @@ function callMeMaybe() {
     console.log(callMe);
   }, 4000);
 }
-callMeMaybe();
+// callMeMaybe();
 //In this case the variable will be after the setTimeOut, but the callMe const is still saved because the SetTimeOut needs it!
 function callMeMaybe2() {
   setTimeout(() => {
@@ -35,4 +35,31 @@ function callMeMaybe2() {
   }, 4000);
   const callMe = "Hi! I am now here!";
 }
-callMeMaybe2();
+// callMeMaybe2();
+
+//WHY USE CLOSURES
+//1. Memory efficient:
+function heavyDuty(index) {
+  const bigArray = new Array(7000).fill(Math.random());
+  console.log("array created in heavyDuty");
+  return bigArray[index];
+}
+// Each time I call the function the bigArray is created
+// console.log(heavyDuty(656));
+// console.log(heavyDuty(66));
+// console.log(heavyDuty(6));
+// With closure
+function heavyDuty2() {
+  const bigArray = new Array(7000).fill(Math.random());
+  console.log("array created in heavyDuty2");
+  return function (index) {
+    return bigArray[index];
+  };
+}
+
+const getHeavyDuty = heavyDuty2();
+// console.log(getHeavyDuty(4));
+// console.log(getHeavyDuty(44));
+// console.log(getHeavyDuty(454));
+
+//2. Encapsulation:
