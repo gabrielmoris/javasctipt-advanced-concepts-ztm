ccf60428db9da9feacdf75ed672dd6373fc7a258
gabriel <gabrielcmoris@gmail.com>
2022-09-28 12:34:25 +0200

    es7

---
+++ extras/es7.js
@@ -0,0 +1,11 @@
+//.includes()
+
+console.log("HolA".includes("a")); //false
+console.log("HolA".includes("H")); //true
+
+//square numbers
+
+const square = (x) => x ** 2;
+const cube = (y) => y ** 3;
+console.log(square(5)); //25
+console.log(cube(5)); //125
